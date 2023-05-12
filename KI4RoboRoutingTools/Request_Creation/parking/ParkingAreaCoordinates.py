12d8480b7c3269836f74dd429980e85c33eddf2e
Steffen Lang <steffenlang1994@gmail.com>
2023-05-12 13:02:04 +0200

    request_creation: parking area - remove and save updated parkingArea.xml in SUMO dir

--- KI4RoboRoutingTools/Request_Creation/parking/ParkingAreaCoordinates.py
+++ KI4RoboRoutingTools/Request_Creation/parking/ParkingAreaCoordinates.py
@@ -5,16 +5,11 @@ import requests
 
 class ParkingAreaCoordinates:
     def __init__(self, xml_file: str):
+        self.xml_file = xml_file
         self.tree = ET.parse(xml_file)
         self.root = self.tree.getroot()
         self.way_ids = [elem.get('id') for elem in self.root.findall('./parkingArea')]
 
-    def remove_parking_area(self, way_id: str) -> bool:
-        parking_area_node = self.root.find(f"./parkingArea[@id='{way_id}']")
-        if parking_area_node is not None:
-            self.root.remove(parking_area_node)
-            return True
-        return False
     def additional_edge_coords(self) -> List[Dict[str, str]]:
         coords = []
         for way_id in self.way_ids:
@@ -23,6 +18,8 @@ class ParkingAreaCoordinates:
                 coords.append({"edge_id": way_id, "lat": lat, "lon": lon})
             except RuntimeError as e:
                 print(e)
+                self.remove_parking_area(way_id)
+        self.tree.write(self.xml_file)
         return coords
 
     def get_way_coordinates(self, way: str) -> Tuple[float, float]:
@@ -37,7 +34,12 @@ class ParkingAreaCoordinates:
         # Überprüfen, ob Koordinaten vorhanden sind
         if len(data) == 0 or 'lat' not in data[0] or 'lon' not in data[0]:
             # Wenn keine Koordinaten gefunden wurden, eine RuntimeError-Exception mit einer Beschreibung werfen
-            self.remove_parking_area(way_id=way)
-            raise RuntimeError(f"Keine Koordinaten gefunden für den Way {way}. Removed it from xml")
+            raise RuntimeError("Keine Koordinaten gefunden für den Way {}".format(way))
         # Koordinaten zurückgeben
         return float(data[0]['lat']), float(data[0]['lon'])
+
+    def remove_parking_area(self, way_id: str):
+        for elem in self.root.findall('./parkingArea'):
+            if elem.get('id') == way_id:
+                self.root.remove(elem)
+                break
