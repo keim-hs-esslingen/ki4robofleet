b580a469f60d829e138d64b92735298a91e8f277
Steffen Lang <steffenlang1994@gmail.com>
2023-05-24 08:57:13 +0200

    prediction model: improve resilience - respect trailing '<edgeid>_0'

--- KI4RoboRoutingTools/Prediction_Model/edge_coordinates.py
+++ KI4RoboRoutingTools/Prediction_Model/edge_coordinates.py
@@ -17,6 +17,13 @@ class EdgeCoordinates:
 
     def get_coord(self, edge_id: str) -> Coordinates:
         for edge_coord in self.__edge_cooridnates:
-            if edge_coord["edge_id"] == edge_id:
-                return edge_coord["coords"]
+            edge_coord_from_list = edge_coord["edge_id"]
+            if len(edge_coord_from_list) == len(edge_id):
+                if edge_coord_from_list == edge_id:
+                    return edge_coord["coords"]
+            elif len(edge_coord_from_list) == len(edge_id) + 2:
+                if edge_coord_from_list[0:len(edge_coord_from_list) - 2:1] == edge_id:
+                    return edge_coord["coords"]
+            else:
+                pass
         raise ValueError(f"edge_id {edge_id} not found in edge_coord_list(len={len(self.__edge_cooridnates)})")
